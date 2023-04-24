91a54a1677ce1900be0c3d215c96c7cf610f8c57
Fabio Medina <disenofabiomedina@gmail.com>
2023-04-24 09:56:10 -0500

    Configuración Redux y Reducer de Auth inicial

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import React from 'react' ;
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { JournalApp } from './JournalApp';
-import './style.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { JournalApp } from "./JournalApp";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
